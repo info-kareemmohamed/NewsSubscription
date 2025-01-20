e853ced22feb6f84140e76165d5533a7780d9893
Kareem Mohamed <63722@fa-hiss.edu.eg>
2025-01-20 09:44:08 +0200

    Pass `canUserReadArticleUseCase` into the news navigation graph.

--- app/src/main/java/com/example/newssubscription/app/navigation/NavGraph.kt
+++ app/src/main/java/com/example/newssubscription/app/navigation/NavGraph.kt
@@ -4,12 +4,12 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.example.newssubscription.authentication.presentation.navigation.addAuthenticationGraph
-import com.example.newssubscription.core.presentation.MainViewModel
+import com.example.newssubscription.core.domain.usecase.CanUserReadArticleUseCase
 import com.example.newssubscription.news.presentation.navigation.addNewsNavigatorGraph
 import com.example.newssubscription.onboarding.presentation.navigation.addOnBoardingGraph
 
 @Composable
-fun NavGraph(startDestination: Routes, viewModel: MainViewModel) {
+fun NavGraph(startDestination: Routes, canUserReadArticleUseCase: CanUserReadArticleUseCase) {
     val navController = rememberNavController()
 
     NavHost(
@@ -20,11 +20,14 @@ fun NavGraph(startDestination: Routes, viewModel: MainViewModel) {
 
         addAuthenticationGraph(navController)
 
-        addNewsNavigatorGraph{
-            navController.navigate(Routes.LoginScreen) {
-                popUpTo(Routes.NewsNavigation) { inclusive = true }
+        addNewsNavigatorGraph(
+            canUserReadArticleUseCase = canUserReadArticleUseCase,
+            authenticationCallback = {
+                navController.navigate(Routes.LoginScreen) {
+                    popUpTo(Routes.NewsNavigation) { inclusive = true }
+                }
             }
-        }
+        )
     }
 }
 
