60a3655fa05df71edec0cf6bb1597f09b121c3c6
Kareem Mohamed <63722@fa-hiss.edu.eg>
2025-01-20 09:42:42 +0200

    Remove `IsUserSignedInUseCase` and use `isUserSignedIn` from `UserRepository` directly in the ViewModel. Also, add `canUserReadArticleUseCase` to be passed into the news navigation graph.

--- app/src/main/java/com/example/newssubscription/core/presentation/MainViewModel.kt
+++ app/src/main/java/com/example/newssubscription/core/presentation/MainViewModel.kt
@@ -7,14 +7,21 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.newssubscription.app.navigation.Routes
 import com.example.newssubscription.core.domain.repository.LocalUserAppEntry
-import com.example.newssubscription.core.domain.usecase.IsUserSignedInUseCase
+import com.example.newssubscription.core.domain.repository.UserRepository
+import com.example.newssubscription.core.domain.usecase.CanUserReadArticleUseCase
+import com.example.newssubscription.core.domain.usecase.CheckPremiumExpirationUseCase
+import com.example.newssubscription.core.domain.usecase.RestartFreeReadDateUseCase
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class MainViewModel @Inject constructor(
-    private val isUserSignedInUseCase: IsUserSignedInUseCase,
+    val canUserReadArticleUseCase: CanUserReadArticleUseCase,
+    private val restartFreeReadDateUseCase: RestartFreeReadDateUseCase,
+    private val checkPremiumExpirationUseCase: CheckPremiumExpirationUseCase,
+    private val userRepository: UserRepository,
     localUserAppEntry: LocalUserAppEntry,
 ) : ViewModel() {
 
@@ -26,6 +33,7 @@ class MainViewModel @Inject constructor(
 
 
     init {
+        viewModelScope.launch { handleUserInitialization() }
         localUserAppEntry.readAppEntry().onEach { startFromNews ->
             setStartDestination(startFromNews)
         }.launchIn(viewModelScope)
@@ -35,11 +43,21 @@ class MainViewModel @Inject constructor(
     private suspend fun setStartDestination(startFromNews: Boolean) {
         if (startFromNews) {
             _startDestination.value =
-                if (isUserSignedInUseCase()) Routes.NewsNavigation else Routes.NewsAuthentication
+                if (userRepository.isUserSignedIn()) Routes.NewsNavigation else Routes.NewsAuthentication
         } else {
             _startDestination.value = Routes.AppStartNavigation
         }
         delay(600) //Without this delay, the onBoarding screen will show for a momentum.
         _keepOnScreenCondition.value = false
     }
+
+
+    private suspend fun handleUserInitialization() {
+        userRepository.getUser()?.let { user ->
+            if (user.premium) checkPremiumExpirationUseCase(user)
+            else restartFreeReadDateUseCase(user)
+        }
+    }
+
+
 }
