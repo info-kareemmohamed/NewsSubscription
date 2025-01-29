80ac497b0c97064b943a7e1de31cdaa6951c826a
Kareem Mohamed <63722@fa-hiss.edu.eg>
2025-01-30 00:02:08 +0200

    Add error messages in the string resources to handle cases such as displaying errors on the empty screen and managing no data errors from the API in the search screen.

--- app/src/main/java/com/example/newssubscription/news/presentation/util/ParseErrorMessage.kt
+++ app/src/main/java/com/example/newssubscription/news/presentation/util/ParseErrorMessage.kt
@@ -1,27 +1,39 @@
 package com.example.newssubscription.news.presentation.util
 
+import android.content.Context
 import androidx.paging.LoadState
+import com.example.newssubscription.R
+import retrofit2.HttpException
 import java.net.ConnectException
 import java.net.SocketTimeoutException
 import java.net.UnknownHostException
 
 
-fun parseErrorMessage(error: LoadState.Error?): String {
-    return when (error?.error) {
+fun parseErrorMessage(error: LoadState.Error?, context: Context): String {
+    return when (val exception = error?.error) {
         is SocketTimeoutException -> {
-            "Server Unavailable."
+            context.getString(R.string.error_server_unavailable)
         }
 
         is ConnectException -> {
-            "Internet Unavailable."
+            context.getString(R.string.error_internet_unavailable)
         }
 
         is UnknownHostException -> {
-            "No internet connection. Please check your connection and try again"
+            context.getString(R.string.error_no_internet_connection)
+        }
+
+        is HttpException -> {
+            when (exception.code()) {
+                426 -> context.getString(R.string.error_no_results_found)
+                404 -> context.getString(R.string.error_no_results_found)
+                204 -> context.getString(R.string.error_no_results_found)
+                else -> context.getString(R.string.error_unknown)
+            }
         }
 
         else -> {
-            "Unknown Error."
+            context.getString(R.string.error_unknown)
         }
     }
 }
